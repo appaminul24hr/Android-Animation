6fe3d9faa6b3e856e3706fa44644d476452c04f2
appaminul24hr <appdeveloperaminul@gmail.com>
2024-03-24 15:53:21 +0600

    first commit

---
+++ app/src/main/java/com/aisoft/androidanimation/MainActivity.java
@@ -0,0 +1,57 @@
+package com.aisoft.androidanimation;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView  animImg;
+    Button animBtn;
+    Animation  myanim;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+        animImg = findViewById(R.id.animImg);
+        animBtn = findViewById(R.id.animBtn);
+        myanim = AnimationUtils.loadAnimation(MainActivity.this,R.anim.my_anim);
+
+
+
+
+
+        animBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImg.startAnimation(myanim);
+            }
+        });
+
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
